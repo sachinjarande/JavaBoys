bb7033349ea131ce751ffc322f4e89e52a87a97f
sachinjarande <sachinjarande71093@gmail.com>
2021-10-09 13:42:40 +0530

    IES:01: Initial created

---
+++ src/main/java/com/bikkadit/JavaBoysFirstProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBoysFirstProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBoysFirstProjectsApplication.class, args);
+	}
+
+}
